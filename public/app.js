c845a1c428940ac076d394c1f7aa96222862df2c
Tyler Briskie <tylerbriskie@gmail.com>
2018-01-12 12:04:22 -0500

    adding event handlers for buttons

--- public/app.js
+++ public/app.js
@@ -1,5 +1,9 @@
-(function() {
-  'use strict';
-  console.log('hi tbone');
-  
-}());
+console.log('hi tbone');
+
+function goToArcade(){
+  console.log('going 2 arcade');
+};
+
+function goToMusic() {
+  console.log('going to music');
+};
